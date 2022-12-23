ce4e65c61dec6dad172dc02f62f17c28d655b0db
CHCheshire <pringl31@gmail.com>
2022-12-23 16:06:02 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 import styles from './App.module.css';
 import NavBar from "./components/NavBar";
 import {Route, Switch} from 'react-router-dom'
-import '.api/axiosDefaults'
+import "./api/axiosDefault"
 import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
