9b313491b97c9a2d5c42c4821eb780cb1d8cb1b6
CHCheshire <pringl31@gmail.com>
2022-12-27 22:35:48 +0000

    Add Contexts to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import { Container } from 'react-bootstrap';
-import styles from './App.module.css';
+import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
-import {Route, Switch} from 'react-router-dom';
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefault";
-import SignUpForm from './pages/auth/SignUpForm';
+import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
   return (
@@ -12,9 +12,10 @@ function App() {
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home Page</h1>} />
-          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
     </div>
