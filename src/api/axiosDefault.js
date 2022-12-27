6ef91056dbdb59714565812ac64a8aa74716642f
CHCheshire <pringl31@gmail.com>
2022-12-27 22:07:10 +0000

    Updated Axios defaults

--- src/api/axiosDefault.js
+++ src/api/axiosDefault.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.BaseURL = 'https://social-space-api.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.WithCredentials = true;
+axios.defaults.baseURL = 'https://social-space-api.herokuapp.com/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
