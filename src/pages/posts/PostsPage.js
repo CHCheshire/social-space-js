da4778cf3d267bf385f9fe377f8962f28eb1ff36
CHCheshire <pringl31@gmail.com>
2023-01-03 12:43:12 +0000

    Fix issue with PostsPage hidden by the banner

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -46,7 +46,7 @@ function PostsPage({ message, filter = "" }) {
   }, [filter, query, pathname]);
 
   return (
-    <Row className="h-100">
+    <Row className={styles.PostsPanel}>
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
         <i className={`fas fa-search ${styles.SearchIcon}`} />
