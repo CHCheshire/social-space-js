0f07dcccb0c3f4e11f505f494c475155655c84df
CHCheshire <pringl31@gmail.com>
2023-03-02 09:16:22 +0000

    Add reviews to App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
+import ReviewCreateForm from "./pages/reviews/ReviewCreateForm";
+import ReviewPage from "./pages/reviews/ReviewPage";
 import PostsPage from "./pages/posts/PostsPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 
@@ -40,6 +42,8 @@ function App() {
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/post/create" render={() => <PostCreateForm />} />
           <Route exact path="/post/:id" render={() => <PostPage />} />
+          <Route exact path="/reviews/create" render={() => <ReviewCreateForm />} />
+          <Route exact path="/reviews/:id" render={() => <ReviewPage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
