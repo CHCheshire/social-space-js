39f9724e2109e1cf78f6f6b28701e165d5655acb
CHCheshire <pringl31@gmail.com>
2023-03-30 14:45:39 +0000

    Update profile pages

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -106,7 +106,7 @@ function ProfilePage() {
               </Button>
             ))}
         </Col>
-        {profile?.content && <Col className="p-3">{profile.content}</Col>}
+        {profile?.bio && <Col className="p-3">{profile.bio}</Col>}
       </Row>
     </>
   );
